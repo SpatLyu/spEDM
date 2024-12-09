5834ee2a0952cadbb49d672228b80cb5dd516ef2
SpatLyu <lyu.geosocial@gmail.com>
2024-12-09 18:17:52 +0800

    add `LinearTrendRM` pure cpp src

--- src/CppStats.h
+++ src/CppStats.h
@@ -10,4 +10,9 @@ double CppSignificance(double r, int n);
 std::vector<double> CppConfidence(double r, int n,
                                   double level = 0.05);
 
+std::vector<double> LinearTrendRM(const std::vector<double>& vec,
+                                  const std::vector<double>& xcoord,
+                                  const std::vector<double>& ycoord,
+                                  bool NA_rm = false);
+
 #endif // CppStats_H
