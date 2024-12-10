5a8ab3d50bab602b142bfbebc4fb266e890754db
SpatLyu <lyu.geosocial@gmail.com>
2024-12-10 11:22:13 +0800

    update default libsizes in `gccm()`

--- R/gccm.R
+++ R/gccm.R
@@ -45,7 +45,8 @@ gccm = \(cause, effect, nbmat = NULL, coords = NULL, data = NULL, libsizes = NUL
   }
 
   if (length(cause) != nrow(nbmat)) stop("Incompatible Data Dimensions!")
-  if (is.null(libsizes)) libsizes = floor(seq(E + 2,length(cause),length.out = 15))
+  if (is.null(libsizes)) libsizes = floor(seq(E + 2,length(cause),
+                                              length.out = floor(sqrt(length(cause)))))
 
   trend_rm = match.arg(trend_rm)
   switch(trend_rm,
