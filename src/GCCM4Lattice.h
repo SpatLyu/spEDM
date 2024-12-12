b7e5a35901ebb331209aac415f2d1a8bebad0ff0
SpatLyu <lyu.geosocial@gmail.com>
2024-12-12 13:30:33 +0800

    rearrange src cpp

--- src/GCCM4Lattice.h
+++ src/GCCM4Lattice.h
@@ -10,16 +10,8 @@
 #include <map>
 #include "CppStats.h"
 #include "CppLatticeUtils.h"
+#include "SimplexProjection.h"
 #include <RcppThread.h>
-
-// Function to compute the simplex projection
-double SimplexProjectionLattice(
-    const std::vector<std::vector<double>>& vectors,  // Reconstructed state-space (each row is a separate vector/state)
-    const std::vector<double>& target,                // Time series to be used as the target (should line up with vectors)
-    const std::vector<bool>& lib_indices,             // Vector of T/F values (which states to include when searching for neighbors)
-    const std::vector<bool>& pred_indices,            // Vector of T/F values (which states to predict from)
-    int num_neighbors                                // Number of neighbors to use for simplex projection
-);
 
 // Function to compute GCCMSingle4Lattice
 std::vector<std::pair<int, double>> GCCMSingle4Lattice(
