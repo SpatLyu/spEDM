db302f92dd01db09c8135bb0307dc876d7bc9c5c
SpatLyu <lyu.geosocial@gmail.com>
2025-01-24 12:49:52 +0800

    update gccm s4 methods for `sf` data input

--- R/RcppExports.R
+++ R/RcppExports.R
@@ -29,7 +29,7 @@ RcppGenLatticeEmbeddings <- function(vec, nb, E) {
     .Call(`_spEDM_RcppGenLatticeEmbeddings`, vec, nb, E)
 }
 
-RcppGCCM4Lattice <- function(x, y, nb, libsizes, E, tau, b, progressbar) {
-    .Call(`_spEDM_RcppGCCM4Lattice`, x, y, nb, libsizes, E, tau, b, progressbar)
+RcppGCCM4Lattice <- function(x, y, nb, libsizes, E, tau, b, simplex, theta, progressbar) {
+    .Call(`_spEDM_RcppGCCM4Lattice`, x, y, nb, libsizes, E, tau, b, simplex, theta, progressbar)
 }
 
