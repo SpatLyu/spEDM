c70ced036ffa332436aa7322f370f791e75984a4
SpatLyu <lyu.geosocial@gmail.com>
2025-01-30 01:28:50 +0800

    update `partial cross mapping` src cpp

---
+++ src/SCPCM4Lattice.h
@@ -0,0 +1,54 @@
+#ifndef SCPCM4Lattice_H
+#define SCPCM4Lattice_H
+
+#include <vector>
+#include <cmath>
+#include <algorithm> // Include for std::partial_sort
+#include <numeric>
+#include <utility>
+#include <limits>
+#include <map>
+#include "CppStats.h"
+#include "CppLatticeUtils.h"
+#include "SimplexProjection.h"
+#include "SMap.h"
+#include <RcppThread.h>
+
+std::vector<std::pair<int, double>> SCPCMSingle4Lattice(
+    const std::vector<std::vector<double>>& x_vectors,  // Reconstructed state-space (each row is a separate vector/state)
+    const std::vector<double>& y,                       // Spatial cross-section series to be used as the target (should line up with vectors)
+    const std::vector<std::vector<double>>& controls,   // Cross-sectional data of control variables (**stored by row**)
+    const std::vector<std::vector<int>>& nb_vec,        // Neighbor indices vector of the spatial units
+    const std::vector<bool>& lib_indices,               // Vector of T/F values (which states to include when searching for neighbors)
+    int lib_size,                                       // Size of the library
+    int max_lib_size,                                   // Maximum size of the library
+    const std::vector<int>& possible_lib_indices,       // Indices of possible library states
+    const std::vector<bool>& pred_indices,              // Vector of T/F values (which states to predict from)
+    const std::vector<int>& conEs,                      // Number of dimensions for the attractor reconstruction with control variables
+    int E,                                              // Number of dimensions for the attractor reconstruction
+    int b,                                              // Number of neighbors to use for simplex projection
+    bool simplex,                                       // Algorithm used for prediction; Use simplex projection if true, and s-mapping if false
+    double theta,                                       // Distance weighting parameter for the local neighbours in the manifold
+    bool includeself                                    // Whether to include the current state when constructing the embedding vector
+);
+
+std::vector<std::vector<double>> SCPCM4Lattice(
+    const std::vector<double>& x,                       // Spatial cross-section series to cross map from
+    const std::vector<double>& y,                       // Spatial cross-section series to cross map to
+    const std::vector<std::vector<double>>& controls,   // Cross-sectional data of control variables (**stored by row**)
+    const std::vector<std::vector<int>>& nb_vec,        // Neighbor indices vector of the spatial units
+    const std::vector<int>& lib_sizes,                  // Vector of library sizes to use
+    const std::vector<std::pair<int, int>>& lib,        // Matrix (n x 2) using n sequences of data to construct libraries
+    const std::vector<std::pair<int, int>>& pred,       // Matrix (n x 2) using n sequences of data to predict from
+    const std::vector<int>& conEs,                      // Number of dimensions for the attractor reconstruction with control variables
+    int E,                                              // Number of dimensions for the attractor reconstruction
+    int tau,                                            // Spatial lag for the lagged-vector construction
+    int b,                                              // Number of nearest neighbors to use for prediction
+    bool simplex,                                       // Algorithm used for prediction; Use simplex projection if true, and s-mapping if false
+    double theta,                                       // Distance weighting parameter for the local neighbours in the manifold
+    int threads,                                        // Number of threads used from the global pool
+    bool includeself,                                   // Whether to include the current state when constructing the embedding vector
+    bool progressbar = true                             // Whether to print the progress bar
+);
+
+#endif // SCPCM4Lattice_H
