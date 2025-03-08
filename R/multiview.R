b9a149e6614e4f25fc709b74557671ee91ab4c10
SpatLyu <lyu.geosocial@gmail.com>
2025-03-08 16:05:01 +0800

    update multiview R API

--- R/multiview.R
+++ R/multiview.R
@@ -1,21 +1,23 @@
 methods::setGeneric("multiview", function(data, ...) standardGeneric("multiview"))
 
-.multiview_sf_method = \(data,columns,target,nvar,lib,pred = lib,E = 3,tau = 1,k = E+2,
+.multiview_sf_method = \(data,columns,target,nvar,lib = NULL,pred = NULL,E = 3,tau = 1,k = E+2,
                          nb = NULL, top = NULL, threads = detectThreads(), trend.rm = TRUE){
   xmat = .multivar_lattice(data,columns,trend.rm)
   yvec = .uni_lattice(data,target,trend.rm)
-  lib = .check_indices(lib,length(yvec))
-  pred = .check_indices(pred,length(yvec))
+  if (is.null(lib)) lib = 1:nrow(data)
+  if (is.null(pred)) pred = lib
   if (is.null(nb)) nb = .internal_lattice_nb(data)
   if (is.null(top)) top = 0
   res = RcppMultiView4Lattice(xmat,yvec,nb,lib,pred,E,tau,k,top,nvar,threads)
   return(res)
 }
 
-.multiview_spatraster_method = \(data,columns,target,nvar,lib,pred = lib,E = 3,tau = 1,k = E+2,
-                                 top = NULL, threads = detectThreads(), trend.rm = TRUE){
+.multiview_spatraster_method = \(data,columns,target,nvar,lib = NULL,pred = NULL,E = 3,tau = 1,
+                                 k = E+2,top = NULL,threads = detectThreads(),trend.rm = TRUE){
   xmat = .multivar_grid(data,columns,trend.rm)
   ymat = .uni_grid(data,target,trend.rm)
+  if (is.null(lib)) lib = .internal_samplemat(ymat)
+  if (is.null(pred)) pred = .internal_samplemat(ymat,floor(sqrt(length(ymat))))
   if (is.null(top)) top = 0
   res = RcppMultiView4Grid(xmat,ymat,lib,pred,E,tau,k,top,nvar,threads)
   return(res)
