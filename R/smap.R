84e3d28669d6bb8418f5a0acda423a6fe8b36c1b
SpatLyu <lyu.geosocial@gmail.com>
2025-03-08 16:02:48 +0800

    update smap R API

--- R/smap.R
+++ R/smap.R
@@ -1,22 +1,24 @@
 methods::setGeneric("smap", function(data, ...) standardGeneric("smap"))
 
-.smap_sf_method = \(data, target, lib, pred = lib, E = 3, tau = 1, k = E+2,
+.smap_sf_method = \(data,target,lib = NULL,pred = NULL,E = 3,tau = 1,k = E+2,
                     theta = c(0, 1e-04, 3e-04, 0.001, 0.003, 0.01, 0.03,
                               0.1, 0.3, 0.5, 0.75, 1, 1.5, 2, 3, 4, 6, 8),
                     nb = NULL, threads = detectThreads(), trend.rm = TRUE){
   vec = .uni_lattice(data,target,trend.rm)
-  lib = .check_indices(lib,length(vec))
-  pred = .check_indices(pred,length(vec))
+  if (is.null(lib)) lib = 1:nrow(data)
+  if (is.null(pred)) pred = lib
   if (is.null(nb)) nb = .internal_lattice_nb(data)
   res = RcppSMap4Lattice(vec,nb,lib,pred,theta,E,tau,k,threads)
   return(.bind_xmapself(res,target))
 }
 
-.smap_spatraster_method = \(data, target, lib, pred = lib, E = 3, tau = 1, k = E+2,
+.smap_spatraster_method = \(data,target,lib = NULL,pred = NULL,E = 3,tau = 1,k = E+2,
                             theta = c(0, 1e-04, 3e-04, 0.001, 0.003, 0.01, 0.03,
                                       0.1, 0.3, 0.5, 0.75, 1, 1.5, 2, 3, 4, 6, 8),
                             threads = detectThreads(), trend.rm = TRUE){
   mat = .uni_grid(data,target,trend.rm)
+  if (is.null(lib)) lib = .internal_samplemat(mat)
+  if (is.null(pred)) pred = .internal_samplemat(mat,floor(sqrt(length(mat))))
   res = RcppSMap4Grid(mat,lib,pred,theta,E,tau,k,threads)
   return(.bind_xmapself(res,target))
 }
