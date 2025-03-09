3e9c596a62b93779aa7e1abe45398ea651dd5af0
SpatLyu <lyu.geosocial@gmail.com>
2025-03-09 14:00:31 +0800

    clean unnecessary src cpp codes

--- src/GCCM4Grid.cpp
+++ src/GCCM4Grid.cpp
@@ -31,6 +31,7 @@
  * @param simplex              If true, use Simplex Projection; if false, use S-Mapping.
  * @param theta                The distance weighting parameter for S-Mapping (ignored if simplex is true).
  * @param threads              The number of threads to use for parallel processing.
+ * @param parallel_level       Level of parallel computing: 0 for `lower`, 1 for `higher`.
  * @param row_size_mark        If true, use the row-wise libsize to mark the libsize; if false, use col-wise libsize.
  *
  * @return  A vector of pairs, where each pair contains the library size and the corresponding cross mapping result.
@@ -47,6 +48,7 @@ std::vector<std::pair<int, double>> GCCMSingle4Grid(
     bool simplex,
     double theta,
     size_t threads,
+    int parallel_level,
     bool row_size_mark) {
   // Extract row-wise and column-wise library sizes
   int lib_size_row = lib_sizes[0];
@@ -194,6 +196,7 @@ std::vector<std::pair<int, double>> GCCMSingle4Grid(
  * @param simplex              Use simplex projection if true, S-mapping if false
  * @param theta                Distance weighting parameter for S-mapping
  * @param threads              The number of threads to use for parallel processing
+ * @param parallel_level       Level of parallel computing: 0 for `lower`, 1 for `higher`
  *
  * @return A vector of pairs, where each pair contains the library size and the corresponding cross mapping result.
  */
@@ -209,7 +212,8 @@ std::vector<std::pair<int, double>> GCCMSingle4GridOneDim(
     int b,
     bool simplex,
     double theta,
-    size_t threads) {
+    size_t threads,
+    int parallel_level) {
   int n = yPred.size();
 
   // No possible library variation if using all vectors
@@ -306,18 +310,19 @@ std::vector<std::pair<int, double>> GCCMSingle4GridOneDim(
  * This function calculates the cross mapping between predictor variables (xMatrix) and response variables (yMatrix)
  * over a 2D grid, using either Simplex Projection or S-Mapping. It supports parallel processing and progress tracking.
  *
- * @param xMatrix      A 2D matrix of the predictor variable's values (spatial cross-section data).
- * @param yMatrix      A 2D matrix of the response variable's values (spatial cross-section data).
- * @param lib_sizes    A 2D vector where the first sub-vector contains row-wise library sizes and the second sub-vector contains column-wise library sizes.
- * @param lib          A vector of pairs representing the indices (row, column) of spatial units to be the library.
- * @param pred         A vector of pairs representing the indices (row, column) of spatial units to be predicted.
- * @param E            The number of dimensions for attractor reconstruction.
- * @param tau          The step of spatial lags for prediction.
- * @param b            The number of nearest neighbors to use for prediction.
- * @param simplex      If true, use Simplex Projection; if false, use S-Mapping.
- * @param theta        The distance weighting parameter for S-Mapping (ignored if simplex is true).
- * @param threads      The number of threads to use for parallel processing.
- * @param progressbar  If true, display a progress bar during computation.
+ * @param xMatrix        A 2D matrix of the predictor variable's values (spatial cross-section data).
+ * @param yMatrix        A 2D matrix of the response variable's values (spatial cross-section data).
+ * @param lib_sizes      A 2D vector where the first sub-vector contains row-wise library sizes and the second sub-vector contains column-wise library sizes.
+ * @param lib            A vector of pairs representing the indices (row, column) of spatial units to be the library.
+ * @param pred           A vector of pairs representing the indices (row, column) of spatial units to be predicted.
+ * @param E              The number of dimensions for attractor reconstruction.
+ * @param tau            The step of spatial lags for prediction.
+ * @param b              The number of nearest neighbors to use for prediction.
+ * @param simplex        If true, use Simplex Projection; if false, use S-Mapping.
+ * @param theta          The distance weighting parameter for S-Mapping (ignored if simplex is true).
+ * @param threads        The number of threads to use for parallel processing.
+ * @param parallel_level Level of parallel computing: 0 for `lower`, 1 for `higher`.
+ * @param progressbar    If true, display a progress bar during computation.
  *
  * @return A 2D vector where each row contains the library size, mean cross mapping result,
  *         significance, and confidence interval bounds.
@@ -334,6 +339,7 @@ std::vector<std::vector<double>> GCCM4Grid(
     bool simplex,
     double theta,
     int threads,
+    int parallel_level,
     bool progressbar
 ) {
   // If b is not provided correctly, default it to E + 2
@@ -511,18 +517,19 @@ std::vector<std::vector<double>> GCCM4Grid(
  * This function calculates the cross mapping between predictor variables (xMatrix) and response variables (yMatrix)
  * over a 2D grid, using either Simplex Projection or S-Mapping. It supports parallel processing and progress tracking.
  *
- * @param xMatrix      A 2D matrix of the predictor variable's values (spatial cross-section data).
- * @param yMatrix      A 2D matrix of the response variable's values (spatial cross-section data).
- * @param lib_sizes    Number of consecutive spatial units to include in each library.
- * @param lib          A vector of representing the indices of spatial units to be the library.
- * @param pred         A vector of representing the indices of spatial units to be predicted.
- * @param E            The number of dimensions for attractor reconstruction.
- * @param tau          The step of spatial lags for prediction.
- * @param b            The number of nearest neighbors to use for prediction.
- * @param simplex      If true, use Simplex Projection; if false, use S-Mapping.
- * @param theta        The distance weighting parameter for S-Mapping (ignored if simplex is true).
- * @param threads      The number of threads to use for parallel processing.
- * @param progressbar  If true, display a progress bar during computation.
+ * @param xMatrix        A 2D matrix of the predictor variable's values (spatial cross-section data).
+ * @param yMatrix        A 2D matrix of the response variable's values (spatial cross-section data).
+ * @param lib_sizes      Number of consecutive spatial units to include in each library.
+ * @param lib            A vector of representing the indices of spatial units to be the library.
+ * @param pred           A vector of representing the indices of spatial units to be predicted.
+ * @param E              The number of dimensions for attractor reconstruction.
+ * @param tau            The step of spatial lags for prediction.
+ * @param b              The number of nearest neighbors to use for prediction.
+ * @param simplex        If true, use Simplex Projection; if false, use S-Mapping.
+ * @param theta          The distance weighting parameter for S-Mapping (ignored if simplex is true).
+ * @param threads        The number of threads to use for parallel processing.
+ * @param parallel_level Level of parallel computing: 0 for `lower`, 1 for `higher`.
+ * @param progressbar    If true, display a progress bar during computation.
  *
  * @return A 2D vector where each row contains the library size, mean cross mapping result,
  *         significance, and confidence interval bounds.
@@ -539,6 +546,7 @@ std::vector<std::vector<double>> GCCM4GridOneDim(
     bool simplex,
     double theta,
     int threads,
+    int parallel_level,
     bool progressbar
 ) {
   // If b is not provided correctly, default it to E + 2
@@ -667,46 +675,7 @@ std::vector<std::vector<double>> GCCM4GridOneDim(
   return final_results;
 }
 
-/* Previously executed in parallel at the lib_sizes level for each lib_size,
- * retained for potential activation.
- *     ----- Wenbo Lv; 2025.03.08
- */
 
-// #include <vector>
-// #include <algorithm>
-// #include <cmath>
-// #include <numeric>
-// #include <utility>
-// #include <limits>
-// #include <map>
-// #include "CppStats.h"
-// #include "CppGridUtils.h"
-// #include "SimplexProjection.h"
-// #include "SMap.h"
-// #include <RcppThread.h>
-//
-// // [[Rcpp::plugins(cpp11)]]
-// // [[Rcpp::depends(RcppThread)]]
-//
-// /**
-//  * Perform Grid-based Geographical Convergent Cross Mapping (GCCM) for a single library size and pred indice.
-//  *
-//  * This function calculates the cross mapping between a predictor variable (xEmbedings) and a response variable (yPred)
-//  * over a 2D grid, using either Simplex Projection or S-Mapping.
-//  *
-//  * @param xEmbedings     A 2D matrix of the predictor variable's embeddings (spatial cross-section data).
-//  * @param yPred          A 1D vector of the response variable's values (spatial cross-section data).
-//  * @param lib_sizes      A vector of two integers, where the first element is the row-wise library size and the second element is the column-wise library size.
-//  * @param pred_indices   A boolean vector indicating which spatial units to be predicted.
-//  * @param totalRow       The total number of rows in the 2D grid.
-//  * @param totalCol       The total number of columns in the 2D grid.
-//  * @param b              The number of nearest neighbors to use for prediction.
-//  * @param simplex        If true, use Simplex Projection; if false, use S-Mapping.
-//  * @param theta          The distance weighting parameter for S-Mapping (ignored if simplex is true).
-//  * @param row_size_mark  If ture, use the row-wise libsize to mark the libsize; if false, use col-wise libsize.
-//  *
-//  * @return  A vector of pairs, where each pair contains the library size and the corresponding cross mapping result.
-//  */
 // std::vector<std::pair<int, double>> GCCMSingle4Grid(
 //     const std::vector<std::vector<double>>& xEmbedings,
 //     const std::vector<double>& yPred,
