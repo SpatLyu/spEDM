ba4ee4d9573d899e1ee99b656b729c80b8020522
SpatLyu <lyu.geosocial@gmail.com>
2025-03-13 15:28:18 +0800

    update cppstats src cpp

--- src/CppStats.cpp
+++ src/CppStats.cpp
@@ -702,7 +702,9 @@ std::vector<double> CppCMCTest(const std::vector<double>& cases,
   double p_value = 2 * R::pnorm(-std::abs(z), 0.0, 1.0, true, false);
 
   // // Compute the one-sided test (right-tailed) p-value (AUC > 0.5)
-  // double p_value = R::pnorm(z, 0.0, 1.0, true, false);
+  // // double p_value = R::pnorm(z, 0.0, 1.0, true, false);
+  // // Set theta to negative when sample size is small to mitigate sample size effect`
+  // double p_value = R::pnorm(-std::abs(z), 0.0, 1.0, true, false);
 
   // Compute the confidence interval using R::qnorm
   double ci_lower = R::qnorm(level / 2, theta, std::sqrt(S), true, false);
