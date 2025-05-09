8a66fd3c89a38b108c5d598f4a5eb06f7dc5ac74
SpatLyu <lyu.geosocial@gmail.com>
2025-05-09 13:34:17 +0800

    use non-NA spatial units for lib/pred by default

--- R/multiview.R
+++ R/multiview.R
@@ -4,7 +4,7 @@ methods::setGeneric("multiview", function(data, ...) standardGeneric("multiview"
                          nb = NULL, top = NULL, threads = detectThreads(), trend.rm = TRUE){
   xmat = .multivar_lattice(data,columns,trend.rm)
   yvec = .uni_lattice(data,target,trend.rm)
-  if (is.null(lib)) lib = .internal_library(data)
+  if (is.null(lib)) lib = .internal_library(cbind(xmat,yvec))
   if (is.null(pred)) pred = lib
   if (is.null(nb)) nb = .internal_lattice_nb(data)
   if (is.null(top)) top = 0
@@ -15,8 +15,8 @@ methods::setGeneric("multiview", function(data, ...) standardGeneric("multiview"
 .multiview_spatraster_method = \(data,columns,target,nvar,lib = NULL,pred = NULL,E = 3,tau = 1,
                                  k = E+2,top = NULL,threads = detectThreads(),trend.rm = TRUE){
   xmat = .multivar_grid(data,columns,trend.rm)
-  ymat = .uni_grid(data,target,trend.rm)
-  if (is.null(lib)) lib = .internal_samplemat(ymat)
+  ymat = .multivar_grid(data,target,trend.rm)
+  if (is.null(lib)) lib = .internal_library(cbind(xmat,ymat))
   if (is.null(pred)) pred = lib
   if (is.null(top)) top = 0
   res = RcppMultiView4Grid(xmat,ymat,lib,pred,E,tau,k,top,nvar,threads)
