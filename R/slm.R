9ba7a0af58763e8ee8d19c17fa287f118dbf59e8
SpatLyu <lyu.geosocial@gmail.com>
2025-06-07 00:40:16 +0800

    feat: return multivariable results in `slm` when given multivariable input

--- R/slm.R
+++ R/slm.R
@@ -8,8 +8,7 @@ methods::setGeneric("slm", function(data, ...) standardGeneric("slm"))
   vy = .uni_lattice(data,y,FALSE)
   vz = .uni_lattice(data,z,FALSE)
   if (is.null(nb)) nb = .internal_lattice_nb(data)
-  return(lapply(RcppSLMTri4Lattice(vx,vy,vz,nb,k,step,alpha_x,alpha_y,alpha_z,beta_xy,beta_xz,beta_yx,beta_yz,beta_zx,beta_zy,threshold),
-                \(.x) apply(.x,1,mean,na.rm = TRUE)))
+  return(.bind_slm(RcppSLMTri4Lattice(vx,vy,vz,nb,k,step,alpha_x,alpha_y,alpha_z,beta_xy,beta_xz,beta_yx,beta_yz,beta_zx,beta_zy,threshold),y,z))
 }
 
 .slm_spatraster_method = \(data, x, y = NULL, z = NULL,
@@ -19,8 +18,7 @@ methods::setGeneric("slm", function(data, ...) standardGeneric("slm"))
   mx = .uni_grid(data,x,FALSE)
   my = .uni_grid(data,y,FALSE)
   mz = .uni_grid(data,z,FALSE)
-  return(lapply(RcppSLMTri4Grid(mx,my,mz,k,step,alpha_x,alpha_y,alpha_z,beta_xy,beta_xz,beta_yx,beta_yz,beta_zx,beta_zy,threshold),
-                \(.x) apply(.x,1,mean,na.rm = TRUE)))
+  return(.bind_slm(RcppSLMTri4Grid(mx,my,mz,k,step,alpha_x,alpha_y,alpha_z,beta_xy,beta_xz,beta_yx,beta_yz,beta_zx,beta_zy,threshold),y,z))
 }
 
 #' spatial logistic map
