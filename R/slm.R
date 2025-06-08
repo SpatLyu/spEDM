6585eb9936ec83d2e7fd1bb6125ecb5d070fcd9f
SpatLyu <lyu.geosocial@gmail.com>
2025-06-08 12:43:40 +0800

    support removal of transient effects in spatial logistic map simulation results

--- R/slm.R
+++ R/slm.R
@@ -3,7 +3,7 @@ methods::setGeneric("slm", function(data, ...) standardGeneric("slm"))
 .slm_sf_method = \(data, x, y = NULL, z = NULL,
                    k = 4, step = 20, alpha_x = 0.625, alpha_y = 0.77, alpha_z = 0.55,
                    beta_xy = 0.05, beta_xz = 0.05, beta_yx = 0.4, beta_yz = 0.4, beta_zx = 0.65, beta_zy = 0.65,
-                   threshold = 1e10, nb = NULL){
+                   threshold = 1e10, transient = 1, nb = NULL){
   vx = .uni_lattice(data,x,FALSE)
   vy = .uni_lattice(data,y,FALSE)
   vz = .uni_lattice(data,z,FALSE)
@@ -14,7 +14,7 @@ methods::setGeneric("slm", function(data, ...) standardGeneric("slm"))
 .slm_spatraster_method = \(data, x, y = NULL, z = NULL,
                            k = 4, step = 20, alpha_x = 0.625, alpha_y = 0.77, alpha_z = 0.55,
                            beta_xy = 0.05, beta_xz = 0.05, beta_yx = 0.4, beta_yz = 0.4, beta_zx = 0.65, beta_zy = 0.65,
-                           threshold = 1e10){
+                           threshold = 1e10, transient = 1){
   mx = .uni_grid(data,x,FALSE)
   my = .uni_grid(data,y,FALSE)
   mz = .uni_grid(data,z,FALSE)
@@ -39,6 +39,7 @@ methods::setGeneric("slm", function(data, ...) standardGeneric("slm"))
 #' @param beta_zx (optional) cross-inhibition from z to x.
 #' @param beta_zy (optional) cross-inhibition from z to y.
 #' @param threshold (optional) set to `NaN` if the absolute value exceeds this threshold.
+#' @param transient (optional) transients to be excluded from the results.
 #' @param nb (optional) neighbours list.
 #'
 #' @return A list
