599664783446d5ff9ec17ca47758cc42c7a828a2
SpatLyu <lyu.geosocial@gmail.com>
2025-10-21 23:05:24 +0800

    enable composite embeddings for non-linear dynamical forecasting in the r side

--- R/smap.R
+++ R/smap.R
@@ -1,5 +1,5 @@
-.smap_sf_method = \(data,column,target,lib = NULL,pred = NULL,E = 3,tau = 1,
-                    k = E+2,style = 1,dist.metric = "L2",dist.average = TRUE,
+.smap_sf_method = \(data,column,target,lib = NULL,pred = NULL,E = 3,tau = 1,k = E+2,
+                    style = 1, stack = 0, dist.metric = "L2", dist.average = TRUE,
                     theta = c(0, 1e-04, 3e-04, 0.001, 0.003, 0.01, 0.03,
                               0.1, 0.3, 0.5, 0.75, 1, 1.5, 2, 3, 4, 6, 8),
                     nb = NULL, threads = detectThreads(), detrend = TRUE){
@@ -8,14 +8,13 @@
   if (is.null(lib)) lib = .internal_library(cbind(vx,vy))
   if (is.null(pred)) pred = lib
   if (is.null(nb)) nb = .internal_lattice_nb(data)
-  res = RcppSMap4Lattice(vx,vy,nb,lib,pred,theta,E,tau,k,style,
-                         .check_distmetric(dist.metric), 
-                         dist.average, threads)
+  res = RcppSMap4Lattice(vx,vy,nb,lib,pred,theta,E,tau,k,style,stack,
+                         .check_distmetric(dist.metric),dist.average, threads)
   return(.bind_xmapself(res,target,"smap"))
 }
 
-.smap_spatraster_method = \(data,column,target,lib = NULL,pred = NULL,E = 3,tau = 1,
-                            k = E+2,style = 1,dist.metric = "L2",dist.average = TRUE,
+.smap_spatraster_method = \(data,column,target,lib = NULL,pred = NULL,E = 3,tau = 1,k = E+2,
+                            style = 1, stack = 0, dist.metric = "L2", dist.average = TRUE,
                             theta = c(0, 1e-04, 3e-04, 0.001, 0.003, 0.01, 0.03,
                                       0.1, 0.3, 0.5, 0.75, 1, 1.5, 2, 3, 4, 6, 8),
                             threads = detectThreads(), detrend = TRUE){
@@ -23,9 +22,8 @@
   my = .uni_grid(data,target,detrend)
   if (is.null(lib)) lib = which(!(is.na(mx) | is.na(my)), arr.ind = TRUE)
   if (is.null(pred)) pred = lib
-  res = RcppSMap4Grid(mx,my,lib,pred,theta,E,tau,k,style,
-                      .check_distmetric(dist.metric),
-                      dist.average,threads)
+  res = RcppSMap4Grid(mx,my,lib,pred,theta,E,tau,k,style,stack,
+                      .check_distmetric(dist.metric),dist.average,threads)
   return(.bind_xmapself(res,target,"smap"))
 }
 
