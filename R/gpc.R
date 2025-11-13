e9d3cd48c575a15e2e4a301535716df35b17ed0a
SpatLyu <lyu.geosocial@gmail.com>
2025-11-13 22:16:27 +0800

    implement robust geographical pattern causality on spatial vector and raster data with detailed result aggregation

--- R/gpc.R
+++ R/gpc.R
@@ -1,5 +1,5 @@
-.gpc_sf_method = \(data, cause, effect, E = 3, k = E+2, tau = 1, style = 1, lib = NULL, pred = NULL, dist.metric = "L2", zero.tolerance = E+2,
-                   relative = TRUE, weighted = TRUE, na.rm = TRUE, threads = detectThreads(), detrend = FALSE, bidirectional = TRUE, nb = NULL){
+.gpc_sf_method = \(data, cause, effect, libsizes = NULL, E = 3, k = E+2, tau = 1, style = 1, lib = NULL, pred = NULL, boot = 9, random = TRUE, seed = 42, dist.metric = "L2", zero.tolerance = k,
+                   relative = TRUE, weighted = TRUE, na.rm = TRUE, threads = detectThreads(), detrend = FALSE, parallel.level = "low", bidirectional = TRUE, progressbar = TRUE, nb = NULL){
   varname = .check_character(cause, effect)
   if (is.null(nb)) nb = .internal_lattice_nb(data)
   cause = .uni_lattice(data,cause,detrend)
@@ -7,51 +7,40 @@
   dist.metric = .check_distmetric(dist.metric)
   if (is.null(lib)) lib = which(!(is.na(cause) | is.na(effect)))
   if (is.null(pred)) pred = lib
-  return(.run_gpc(cause, effect, E, k, tau, style, lib, pred,
-                  dist.metric, zero.tolerance,
-                  relative, weighted, na.rm, threads,
-                  bidirectional, varname, nb))
+  return(.run_gpc(cause, effect, E, k, tau, style, lib, pred, dist.metric,
+                  zero.tolerance, relative, weighted, na.rm, threads, bidirectional,
+                  varname, nb, libsizes, boot, random, seed, parallel.level, progressbar))
 }
 
-.gpc_spatraster_method = \(data, cause, effect, E = 3, k = E+2, tau = 1, style = 1, lib = NULL, pred = NULL, dist.metric = "L2", zero.tolerance = E+2,
-                           relative = TRUE, weighted = TRUE, na.rm = TRUE, threads = detectThreads(), detrend = FALSE, bidirectional = TRUE, grid.coord = TRUE){
+.gpc_spatraster_method = \(data, cause, effect, libsizes = NULL, E = 3, k = E+2, tau = 1, style = 1, lib = NULL, pred = NULL, boot = 9, random = TRUE, seed = 42, dist.metric = "L2", zero.tolerance = k,
+                           relative = TRUE, weighted = TRUE, na.rm = TRUE, threads = detectThreads(), detrend = FALSE, parallel.level = "low", bidirectional = TRUE, progressbar = TRUE, grid.coord = TRUE){
   varname = .check_character(cause, effect)
   cause = .uni_grid(data,cause,detrend,grid.coord)
   effect = .uni_grid(data,effect,detrend,grid.coord)
   if (is.null(lib)) lib = which(!(is.na(cause) | is.na(effect)), arr.ind = TRUE)
   if (is.null(pred)) pred = lib
-  return(.run_gpc(cause, effect, E, k, tau, style, lib, pred,
-                  dist.metric, zero.tolerance,
-                  relative, weighted, na.rm, threads,
-                  bidirectional, varname))
+  return(.run_gpc(cause, effect, E, k, tau, style, lib, pred, dist.metric,
+                  zero.tolerance, relative, weighted, na.rm, threads, bidirectional,
+                  varname, NULL, libsizes, boot, random, seed, parallel.level, progressbar))
 }
 
 #' geographical pattern causality
 #'
-#' @param data observation data.
-#' @param cause name of causal variable.
-#' @param effect name of effect variable.
-#' @param E (optional) embedding dimension.
-#' @param k (optional) number of nearest neighbors used in cross mapping.
-#' @param tau (optional) step of spatial lag.
-#' @param style (optional) embedding style (`0` includes current state, `1` excludes it).
-#' @param lib (optional) libraries indices (input needed: `vector` - spatial vector, `matrix` - spatial raster).
-#' @param pred (optional) predictions indices (input requirement same as `lib`).
-#' @param dist.metric (optional) distance metric (`L1`: Manhattan, `L2`: Euclidean).
+#' @inheritParams gcmc
+#' @param boot (optional) number of bootstraps to perform.
+#' @param seed (optional) random seed.
+#' @param random (optional) whether to use random sampling.
 #' @param zero.tolerance (optional) maximum number of zeros tolerated in signature space.
 #' @param relative (optional) whether to calculate relative changes in embeddings.
 #' @param weighted (optional) whether to weight causal strength.
 #' @param na.rm (optional) whether to remove `NA` samples in symbolic pattern generation.
-#' @param threads (optional) number of threads to use.
-#' @param detrend (optional) whether to remove the linear trend.
-#' @param bidirectional (optional) whether to examine bidirectional causality.
-#' @param nb (optional) neighbours list.
 #'
 #' @return A list
 #' \describe{
-#' \item{\code{causality}}{per-sample causality statistics}
-#' \item{\code{summary}}{overall causal strength}
-#' \item{\code{pattern}}{pairwise pattern relationships}
+#' \item{\code{xmap}}{cross mapping results (only present if `libsizes` is not `NULL`)}
+#' \item{\code{causality}}{per-sample causality statistics(present if `libsizes` is `NULL`)}
+#' \item{\code{summary}}{overall causal strength(present if `libsizes` is `NULL`)}
+#' \item{\code{pattern}}{pairwise pattern relationships(present if `libsizes` is `NULL`)}
 #' \item{\code{varname}}{names of causal and effect variable}
 #' }
 #' @export
