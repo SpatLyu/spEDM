2df85e13f60d0c840bf89f05c0553a40a1cc5697
SpatLyu <lyu.geosocial@gmail.com>
2026-01-12 00:06:18 +0800

    use inst/include/spEDM.h to enfore include order in RcppExport.cpp

---
+++ inst/include/spEDM.h
@@ -0,0 +1,5 @@
+// This file will be include into the generated file RcppExports.cpp
+// allowing us to specifiy include order, and (if needed) declare types
+
+#include <RcppArmadillo.h>
+#include <RcppThread.h>
