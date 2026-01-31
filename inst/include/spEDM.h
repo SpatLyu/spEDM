0472d294dea08ef74df4605c904ed61a92493bdb
SpatLyu <lyu.geosocial@gmail.com>
2026-01-31 21:25:30 +0800

    implement include guard in spEDM package header and still enfore include order when export rcpp wrappers

--- inst/include/spEDM.h
+++ inst/include/spEDM.h
@@ -1,5 +1,10 @@
+#ifndef spEDM_H
+#define spEDM_H
+
 // This file will be include into the generated file RcppExports.cpp
-// allowing us to specifiy include order, and (if needed) declare types
+// allowing us to specify include order, and (if needed) declare types
 
 #include <RcppArmadillo.h>
 #include <RcppThread.h>
+
+#endif // spEDM_H
