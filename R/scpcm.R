6cd1a3ce43361231d6e67d94fdb5ea3bf536e31e
SpatLyu <lyu.geosocial@gmail.com>
2026-02-06 16:23:51 +0800

    unify preprocessing strategies for embedding parameters `E` and `tau` across spatial causal discovery algorithms

--- R/scpcm.R
+++ R/scpcm.R
@@ -1,9 +1,9 @@
 .scpcm_sf_method = \(data, cause, effect, conds, libsizes = NULL, E = 3, k = E+2, tau = 1, style = 1, stack = FALSE, lib = NULL, pred = NULL, dist.metric = "L2", dist.average = TRUE,
                      theta = 1, algorithm = "simplex", threads = detectThreads(), detrend = TRUE, parallel.level = "low", bidirectional = TRUE, progressbar = TRUE, cumulate = FALSE, nb = NULL){
   varname = .check_character(c(cause, effect, conds))
-  E = .check_inputelementnum(E,length(varname),2*length(conds))
-  tau = .check_inputelementnum(tau,length(varname),2*length(conds))
-  k = .check_inputelementnum(k,length(varname),2*length(conds))
+  E = .check_inputelementnum(E,length(varname)+length(conds),2*length(conds))
+  tau = .check_inputelementnum(tau,length(varname)+length(conds),2*length(conds))
+  k = .check_inputelementnum(k,length(varname)+length(conds),2*length(conds))
   pl = .check_parallellevel(parallel.level)
   .varname = .internal_varname(conds)
   if (is.null(nb)) nb = .internal_lattice_nb(data)
