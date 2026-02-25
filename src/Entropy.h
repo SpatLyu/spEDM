c571f94983062440215b73672c0b8fad6fb59e4d
SpatLyu <lyu.geosocial@gmail.com>
2026-02-26 00:00:01 +0800

    clean up source code to maintain better readability and hygiene

--- src/Entropy.h
+++ src/Entropy.h
@@ -12,38 +12,38 @@
 #include "CppDistances.h"
 
 double CppEntropy_Cont(const std::vector<double>& vec, size_t k,
-                       double base = 10, bool NA_rm = false);
+                       double base = 10, bool na_rm = false);
 
 double CppJoinEntropy_Cont(const std::vector<std::vector<double>>& mat,
                            const std::vector<int>& columns, size_t k,
-                           double base = 10, bool NA_rm = false);
+                           double base = 10, bool na_rm = false);
 
 double CppMutualInformation_Cont(const std::vector<std::vector<double>>& mat,
                                  const std::vector<int>& columns1,
                                  const std::vector<int>& columns2,
                                  size_t k, int alg = 1,
-                                 bool normalize = false, bool NA_rm = false);
+                                 bool normalize = false, bool na_rm = false);
 
 double CppConditionalEntropy_Cont(const std::vector<std::vector<double>>& mat,
                                   const std::vector<int>& target_columns,
                                   const std::vector<int>& conditional_columns,
-                                  size_t k, double base = 10, bool NA_rm = false);
+                                  size_t k, double base = 10, bool na_rm = false);
 
 double CppEntropy_Disc(const std::vector<double>& vec,
-                       double base = 10, bool NA_rm = false);
+                       double base = 10, bool na_rm = false);
 
 double CppJoinEntropy_Disc(const std::vector<std::vector<double>>& mat,
                            const std::vector<int>& columns,
-                           double base = 10, bool NA_rm = false);
+                           double base = 10, bool na_rm = false);
 
 double CppMutualInformation_Disc(const std::vector<std::vector<double>>& mat,
                                  const std::vector<int>& columns1,
                                  const std::vector<int>& columns2,
-                                 double base = 10, bool NA_rm = false);
+                                 double base = 10, bool na_rm = false);
 
 double CppConditionalEntropy_Disc(const std::vector<std::vector<double>>& mat,
                                   const std::vector<int>& target_columns,
                                   const std::vector<int>& conditional_columns,
-                                  double base = 10, bool NA_rm = false);
+                                  double base = 10, bool na_rm = false);
 
 #endif // Entropy_H
