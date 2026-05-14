63f9177f8da7f740c9f8eaf71070a8a4e958b264
SpatLyu <lyu.geosocial@gmail.com>
2026-05-15 00:00:01 +0800

    correct mismatch between causal strength direction and prediction direction in pattern causality

--- R/gpc.R
+++ R/gpc.R
@@ -51,10 +51,10 @@
 #' @examples
 #' columbus = sf::read_sf(system.file("case/columbus.gpkg",package="spEDM"))
 #' \donttest{
-#' spEDM::gpc(columbus,"crime","hoval",E = 5,k = 7)
+#' spEDM::gpc(columbus,"crime","hoval",E = 7,k = 9)
 #'
 #' # convergence diagnostics
-#' g = spEDM::gpc(columbus,"crime","hoval",libsizes = seq(5,45,5),E = 5,k = 7)
+#' g = spEDM::gpc(columbus,"crime","hoval",libsizes = seq(5,45,5),E = 7,k = 9)
 #' plot(g)
 #' }
 methods::setMethod("gpc", "sf", .gpc_sf_method)
