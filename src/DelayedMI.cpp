3ccb397fddefd860eddec8b5d7c16b0aa7f576d7
SpatLyu <lyu.geosocial@gmail.com>
2026-07-30 00:00:01 +0800

    draft delayed mutual information with pure cpp for spatial lag step selection

--- src/DelayedMI.cpp
+++ src/DelayedMI.cpp
@@ -18,8 +18,8 @@ double KSGMI(
     size_t k = 3,
     size_t alg = 0,
     double base = 2.0,
-    bool normalize = false)
-{
+    bool normalize = false
+){
     const size_t n = d_xy.size();
 
     double sum = 0.0;
@@ -99,4 +99,44 @@ double KSGMI(
     } 
 
     return mival / hxy;
+}
+
+std::vector<double> CppDMI(const std::vector<std::vector<double>>& embedding,
+                           const std::vector<size_t>& lib,
+                           const std::vector<size_t>& pred,
+                           size_t k = 3,
+                           size_t alg = 0,
+                           double base = 2.0,
+                           bool normalize = false
+                           int parallel_level = 0){
+  // Configure threads
+  size_t threads_sizet = static_cast<size_t>(std::abs(threads));
+  threads_sizet = std::min(static_cast<size_t>(std::thread::hardware_concurrency()), threads_sizet);
+
+  size_t max_E2 = embedding[0].size();
+  std::vector<double> results(max_E2 - 1, std::numeric_limits<double>::quiet_NaN());
+
+  if (embedding.empty() || embedding[0].size() < 2) {
+    return results;  // Not enough dimensions to compute FNN
+  }
+
+  if (parallel_level == 0){
+    // Loop through E1 = 1 to max_E2 - 1
+    for (size_t E1 = 1; E1 < max_E2; ++E1) {
+      size_t E2 = E1 + 1;
+      double fnn_ratio = CppSingleFNN(embedding, lib, pred, E1, E2, threads_sizet,
+                                      parallel_level, Rtol[E1 - 1], Atol[E1 - 1], L1norm);
+      results[E1 - 1] = fnn_ratio;
+    }
+  } else {
+    // Parallel computation
+    RcppThread::parallelFor(1, max_E2, [&](size_t E1) {
+      size_t E2 = E1 + 1;
+      double fnn_ratio = CppSingleFNN(embedding, lib, pred, E1, E2, threads_sizet,
+                                      parallel_level, Rtol[E1 - 1], Atol[E1 - 1], L1norm);
+      results[E1 - 1] = fnn_ratio;
+    }, threads_sizet);
+  }
+
+  return results;
 }
